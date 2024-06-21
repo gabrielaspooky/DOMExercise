2fbda6102628429fa55b3012578b358b665a80b1
gabrielaspooky <gabrielanava01@gmail.com>
2024-06-21 17:52:55 +0200

    added new changes

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
+// Ejercicio botones
 
+const section = document.querySelector('section');
 
+for (let i = 1; i <= 100; i++) {
 
+const button = document.createElement('button');
+    
+ button.textContent = i;
+    
 
+if (i % 2 === 0) {
+        button.style.backgroundColor = 'blue';
+    } else {
+        button.style.backgroundColor = 'red';
 
+    }
 
-// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+if (i % 5 === 0) {
+        button.style.color = 'green';
+    }
+    
+    section.appendChild(button);
+}
+
+// Ejercicio arcoiris 
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const spans = document.querySelectorAll('span');
+
+spans.forEach((span, index) => {
+    span.style.color = colors[index];
+});
